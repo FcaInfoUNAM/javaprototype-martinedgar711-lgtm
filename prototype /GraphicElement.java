0d530e629e723ff622b9a10c31c4f19ddb3cc861
martinedgar711-lgtm <martinedgar711@gmail.com>
2026-01-13 16:43:53 -0600

    prototype / GraphicElement.java
    cambios guardados

---
+++ prototype /GraphicElement.java
@@ -0,0 +1,43 @@
+package prototype;
+
+public class GraphicElement implements Cloneable {
+    private String shape;
+    private String color;
+    private int x, y;
+
+    // Expensive constructor (simulated with a Thread.sleep)
+    public GraphicElement(String shape, String color, int x, int y) {
+        this.shape = shape;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        try {
+            // Simulate expensive setup (e.g., loading resources, complex calculations)
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Expensive GraphicElement constructor called");
+    }
+     @Override
+    public String toString() {
+        return String.format("GraphicElement [shape=%s, color=%s, x=%d, y=%d]", shape, color, x, y);
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    @Override
+    public GraphicElement clone() {
+        try {
+            return (GraphicElement) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // No puede ocurrir ya que implementamos Cloneable
+        }
+    }
+}
+// este es el cambio que realize y guarde directamente ya que el codespaces no me deja hacer los cambios
+    // ... getters and setters for the fields ...
